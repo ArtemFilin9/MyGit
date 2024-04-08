6def1ede00a273e044ac3f6bef6214711ec7cf81
artemfilin <artem_filin_2005@mail.ru>
2024-04-08 14:57:51 +0300

    Добавил новую функцию
    Добавил в программу функцию `fib(n)`, которая будет рекурсивно вычислять n-ое число Фибоначчи
    при условии, что последовательность начинается с (1, 1, ...).

--- Репозиторий_с_линейной_историей/HelloWorld.cpp
+++ Репозиторий_с_линейной_историей/HelloWorld.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+int fib(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fib(n-1) + fib(n-2);
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+	int n;
+	std::cin >> n;
+
+	std::cout << "Число Фибоначчи для n = " << n << ": " << fib(n) << std::endl;
+
+	std::cout << "Hello, World!" << std::endl;
+
+	return 0;
 }
