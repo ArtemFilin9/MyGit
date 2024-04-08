5e00e89a03deb86dad01eeb3a404537608ecdff6
artemfilin <artem_filin_2005@mail.ru>
2024-04-08 15:25:30 +0300

    Изменил функцию вычисления чисел фибоначи и вид самой программы
    Исправил программу так, чтобы последовательность чисел начиналась со значений (0, 1, ...).
    Заменил табуляции 4 пробелами

--- Репозиторий_с_линейной_историей/HelloWorld.cpp
+++ Репозиторий_с_линейной_историей/HelloWorld.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 
 int fib(int n) {
-    if (n <= 1) {
-        return n;
-    }
+    if (n == 1) return 0;
+    if (n == 2) return 1;
     return fib(n-1) + fib(n-2);
 }
 
 int main() {
-	int n;
-	std::cin >> n;
+    int n;
+    std::cin >> n;
 
-	std::cout << "Число Фибоначчи для n = " << n << ": " << fib(n) << std::endl;
+    std::cout << "Число Фибоначчи для n = " << n << ": " << fib(n) << std::endl;
 
-	std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
 
-	return 0;
+    return 0;
 }
